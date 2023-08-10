d8dcd4b16830f9b34c73dc7fe28ebed882a77eb9
Shahzod999 <120028419+Shahzod999@users.noreply.github.com>
2023-08-10 11:50:11 +0500

    Update wsgi.py

--- market/market/wsgi.py
+++ market/market/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+#
+## assuming your django settings file is at '/home/Shahzod999/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/Shahzod999/mysite/manage.py'
+path = '/home/Shahzod999/Mert/market'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'market.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market.settings')
-
 application = get_wsgi_application()
+
+from dotenv import load_dotenv # load_dotenv для загрузки переменных окружения 
+project_folder = os.path.expanduser('/home/Shahzod999/Mert/market/market')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
